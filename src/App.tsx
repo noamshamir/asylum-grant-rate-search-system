8c0b29a1eae81872599936074d960c8991256ec2
noamshamir <noamshamir09@gmail.com>
2025-01-16 17:10:00 -0600

    fixed routing!

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
     };
 
     return (
-        <Router>
+        <Router basename='/asylum_grant_rate_search_system'>
             <div className='App' style={{ height: "calc(100vh - 84px)" }}>
                 <Header
                     toggleChat={toggleChat}
